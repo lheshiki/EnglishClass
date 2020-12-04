7fad46fcd42058a17c9bb030b46eec9959271d28
Luis Heshiki <luisheshiki22@gmail.com>
2020-12-04 16:42:35 +0900

    Fixed !

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&amp;display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css"> 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/photogallery.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/selfintroduction.css">
 </head>
 <body class="body">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
